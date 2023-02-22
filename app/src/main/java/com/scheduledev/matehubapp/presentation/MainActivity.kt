33e2b262890e9554c40de5b59c5846a357c96468
Anton V <a.voronkin@altessa-s.com>
2023-02-22 17:45:03 +0700

    add data module

--- app/src/main/java/com/scheduledev/matehubapp/presentation/MainActivity.kt
+++ app/src/main/java/com/scheduledev/matehubapp/presentation/MainActivity.kt
@@ -1,8 +1,8 @@
 package com.scheduledev.matehubapp.presentation
 
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -12,7 +12,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.scheduledev.matehubapp.ui.theme.MateHubAppTheme
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
