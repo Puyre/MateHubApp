6dfc063c38f1317538a34fe2e9b72a5f51bdbb7b
Anton V <a.voronkin@altessa-s.com>
2023-02-27 16:40:33 +0700

    add auth usecase

--- app/src/main/java/com/scheduledev/matehubapp/di/modules/UseCasesModule.kt
+++ app/src/main/java/com/scheduledev/matehubapp/di/modules/UseCasesModule.kt
@@ -1,7 +1,15 @@
 package com.scheduledev.matehubapp.di.modules
 
+import com.sheduledev.domain.repo.AuthRepository
+import com.sheduledev.domain.usecases.AuthUseCase
 import dagger.Module
+import dagger.Provides
+import javax.inject.Singleton
 
 @Module
 class UseCasesModule {
+
+    @Provides
+    @Singleton
+    fun provideAuthUseCase(authRep: AuthRepository) = AuthUseCase(authRep)
 }
---
+++ domain/src/main/java/com/sheduledev/domain/usecases/AuthUseCase.kt
@@ -0,0 +1,11 @@
+package com.sheduledev.domain.usecases
+
+import com.sheduledev.domain.model.TokenPair
+import com.sheduledev.domain.repo.AuthRepository
+
+class AuthUseCase(private val authRep: AuthRepository) {
+
+    suspend fun performAuth(login: String, password: String): TokenPair {
+        return authRep.performAuth(login = login, password = password)
+    }
+}
