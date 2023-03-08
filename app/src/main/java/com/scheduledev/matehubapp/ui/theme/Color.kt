8feef14ed52242e016054483d02dcc4a7f676e21
andrejdonskov <andnskv@gmail.com>
2023-03-08 19:08:13 +0600

    extend auth screen view

--- app/src/main/java/com/scheduledev/matehubapp/ui/theme/Color.kt
+++ app/src/main/java/com/scheduledev/matehubapp/ui/theme/Color.kt
@@ -2,9 +2,9 @@ package com.scheduledev.matehubapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Primary = Color(0xFF123123)
+val Secondary = Color(0xFF215A40)
+val Tertiary = Color(0xFF41B17E)
 
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
--- auth/src/main/java/com/scheduledev/auth/login/LoginScreen.kt
+++ auth/src/main/java/com/scheduledev/auth/login/LoginScreen.kt
@@ -1,20 +1,30 @@
 package com.scheduledev.auth.login
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LoginScreen() {
-
-    //добавляет логику по работе с экраном (scaffold)
     Scaffold(topBar = { Toolbar() }, content = { paddingValues ->
 
         Body(paddingValues)
@@ -39,7 +49,6 @@ fun Toolbar() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Body(rootPadding: PaddingValues) {
-
     Column(
         modifier = Modifier
             .fillMaxHeight()
@@ -47,20 +56,63 @@ fun Body(rootPadding: PaddingValues) {
             .padding(rootPadding)
             .padding(16.dp)
     ) {
+        val emailText = remember { mutableStateOf("") }
+        val passwordText = remember { mutableStateOf("") }
+        val showPassword = remember { mutableStateOf(false) }
+
         OutlinedTextField(
-            value = "",
-            onValueChange = {},
+            value = emailText.value,
+            onValueChange = {
+                emailText.value = it
+            },
             label = { Text(text = "Email") },
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier.fillMaxWidth(),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+            singleLine = true,
+            shape = RoundedCornerShape(12.dp)
         )
         OutlinedTextField(
-            value = "",
-            onValueChange = {},
+            value = passwordText.value,
+            onValueChange = {
+                passwordText.value = it
+            },
             label = { Text(text = "Password") },
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(top = 12.dp)
+                .padding(top = 12.dp),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+            singleLine = true,
+            visualTransformation = if (showPassword.value) VisualTransformation.None else PasswordVisualTransformation(),
+            trailingIcon = {
+                val image = if (showPassword.value) {
+                    Icons.Filled.VisibilityOff
+                } else {
+                    Icons.Filled.Visibility
+                }
+
+                val description = if (showPassword.value) "Hide password" else "Show password"
+
+                IconButton(onClick = { showPassword.value = !showPassword.value }) {
+                    Icon(imageVector = image, description, tint = Color(0xFF123123))
+                }
+            },
+            shape = RoundedCornerShape(12.dp)
         )
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            OutlinedButton(
+                border = BorderStroke(width = 1.dp, color = Color(0xFF123123)),
+                onClick = {
+
+                },
+                modifier = Modifier
+                    .padding(top = 12.dp)
+            ) {
+                Text(text = "Здарова заебал", color = Color(0xFF123123))
+            }
+        }
 
     }
 }
